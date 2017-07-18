123cc02dae4895155942114b8c09a262d71e6c22
Olexander <knoxille@zeoalliance.com>
2017-07-18 16:23:06 +0300

    first

---
+++ js/source/function.js
@@ -0,0 +1,56 @@
+$( document ).ready(function() {
+    $('.js-btn').click(function () {
+        table = $('.js-about-hide');
+        table.toggleClass('active-table');
+        if (table.hasClass('active-table')){
+            $('.js-btn .change-text').text('Свернуть');
+            $('.js-ocon').css({
+                transform: 'rotate(-90deg)'
+            })
+            table.animate({
+                height: 736
+            }, 800)
+        }
+        else {
+            $('.js-ocon').css({
+                transform: 'rotate(0deg)'
+            })
+            $('.js-btn .change-text').text('Узнать больше');
+            table.animate({
+                height: 0
+            }, 800)
+        }
+    })
+    $('a[href^="#"]').click(function(){
+        var el = $(this).attr('href'),
+            link = $('.top__header__list__item a');
+        link.each(function () {
+            $(this).removeClass('active-link')
+        });
+        $(this).addClass('active-link');
+        $('body').animate({
+            scrollTop: $(el).offset().top - 60
+        }, 1000);
+        return false;
+    });
+    $(window).scroll(function()
+    {
+        if($(window).scrollTop() > 100){
+            $('.js-header').addClass('active-scroll');
+        }
+        else {
+            $('.js-header').removeClass('active-scroll');
+        }
+    });
+    $('.js-vacansy').click(function () {
+        $('.you-need').each(function () {
+            $(this).css({
+                display: 'none'
+            })
+        });
+        $('.you-need').eq($(this).index()).css({
+            display: 'table'
+        })
+
+    });
+});
